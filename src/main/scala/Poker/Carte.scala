6a7c73e58c6caec68f4c85fa4fb503b1495ae8d9
LoganDej <logan.de-jesus@etu.umontpellier.fr>
2022-03-24 15:38:14 +0100

    occurence counter

--- src/main/scala/Poker/Carte.scala
+++ src/main/scala/Poker/Carte.scala
@@ -40,7 +40,13 @@ package Poker
         case Nombre.DAME => 11
         case Nombre.ROI => 12
       }
+
+      def getnombre = this.nombre;
+
+      def getcouleur = this.couleur;
     }
+
+
 
 class PokerHand(val cartes: List[PlayingCard]){
 
@@ -56,15 +62,23 @@ class PokerHand(val cartes: List[PlayingCard]){
     case STRAIGHTFLUSH
     case ROYALFLUSH
 
-  def allCurrentCards: List[PlayingCard] = cartes::PokerFlop.cartes::PokerRiver.cartes
-  def occurenceCounter(l: List[PlayingCard], v: valeur, Int: curr): Int = a.head.valueOf match {
-    case valeur => occurenceCounter(l.tail,v+1)
-    case Nil => 0
-    case _ => occurenceCounter(l.tail,v)
+  //def allCurrentCards: List[PlayingCard] = cartes::PokerFlop.cartes::PokerRiver.cartes
+
+  // Compte le nombre de cartes avec le meme "Carte.nombre" donné en argument dans une liste de cartes
+  def occurenceNombreCounter(l: List[PlayingCard], v: Nombre, curr:Int): Int = l match {
+    case Nil => curr
+    case x::xs => if(x.getnombre==v) occurenceNombreCounter(xs,v,curr+1) else occurenceNombreCounter(xs,v,curr)
   }
 
+  // Compte le nombre de cartes avec le meme "Carte.couleur" donné en argument dans une liste de cartes
+  def occurenceCouleurCounter(l: List[PlayingCard], v: Couleur, curr:Int): Int = l match {
+    case Nil => curr
+    case x::xs => if(x.getcouleur==v) occurenceCouleurCounter(xs,v,curr+1) else occurenceCouleurCounter(xs,v,curr)
+  }
+
+
   /*Paire*/
-  def isPair: Boolean = occurenceCounter(allCurrentCards,allCurrentCards.head.valueOf,0)
+  def isPair: Boolean = occurenceNombreCounter(cartes,Nombre.DEUX,0)==2                 //occurenceCounter(allCurrentCards,allCurrentCards.head.valueOf,0)
 
   /*Deux Paires*/
   def isTwoPair: Boolean = true
@@ -109,10 +123,17 @@ object ComparePC extends scala.math.Ordering[PlayingCard] {
 
 
   @main def main =
-    println(ComparePC.compare(PlayingCard(Nombre.DEUX,Couleur.PIQUE),PlayingCard(Nombre.QUATRE,Couleur.COEUR)))
-    println(ComparePC.compare(PlayingCard(Nombre.HUIT,Couleur.PIQUE),PlayingCard(Nombre.QUATRE,Couleur.COEUR)))
-    println(ComparePC.compare(PlayingCard(Nombre.AS,Couleur.PIQUE),PlayingCard(Nombre.ROI,Couleur.COEUR)))
-    println(ComparePC.compare(PlayingCard(Nombre.AS,Couleur.PIQUE),PlayingCard(Nombre.AS,Couleur.COEUR)))
+
+    def l: List[PlayingCard] = List(PlayingCard(Nombre.DEUX,Couleur.PIQUE),PlayingCard(Nombre.DEUX,Couleur.COEUR),PlayingCard(Nombre.TROIS,Couleur.COEUR))
+    println(PokerHand(l).occurenceNombreCounter(l,Nombre.TROIS,0))
+    println(PokerHand(l).occurenceCouleurCounter(l,Couleur.COEUR,0))
+
+
+
+  /*println(ComparePC.compare(PlayingCard(Nombre.DEUX,Couleur.PIQUE),PlayingCard(Nombre.QUATRE,Couleur.COEUR)))
+  println(ComparePC.compare(PlayingCard(Nombre.HUIT,Couleur.PIQUE),PlayingCard(Nombre.QUATRE,Couleur.COEUR)))
+  println(ComparePC.compare(PlayingCard(Nombre.AS,Couleur.PIQUE),PlayingCard(Nombre.ROI,Couleur.COEUR)))
+  println(ComparePC.compare(PlayingCard(Nombre.AS,Couleur.PIQUE),PlayingCard(Nombre.AS,Couleur.COEUR)))*/
 
 }
 
@@ -121,6 +142,6 @@ object ComparePH extends scala.math.Ordering[PokerHand]{
 
 }
 
-object Main:
-  @main def main =
-    val sus = 0
+
+
+
